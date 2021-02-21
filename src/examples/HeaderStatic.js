348be5626e6ea9e40c69390810817d42c10ff480
mmaciasjr <mariomacias29@gmail.com>
2021-02-21 04:50:42 -0800

    added static query and added import from HeaderStatic.

--- src/examples/HeaderStatic.js
+++ src/examples/HeaderStatic.js
@@ -1,23 +1,27 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 
-const ComponentName = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
-
-export const query = graphql`
-  {
-    site {
-      info: siteMetadata {
-        person {
-          age
-          name
+const ComponentName = () => (
+  <StaticQuery
+    //graph QL query 
+    query={graphql`
+      {
+        site {
+          info: siteMetadata {
+            person {
+              age
+              name
+            }
+            author
+            data
+            description
+            title
+          }
         }
-        author
-        data
-        description
-        title
       }
-    }
-  }
-`
+    `}
+    render={data => <pre>{JSON.stringify(data, null, 4)}</pre>}
+  ></StaticQuery>
+)
 
 export default ComponentName
